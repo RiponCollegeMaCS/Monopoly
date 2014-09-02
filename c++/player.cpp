7f16d45a0385984e2ff228d5b569432d534442e2
Braxton Schafer <braxton.schafer@gmail.com>
2014-09-02 13:14:18 -0500

    Add constructor to C++ Player class

--- c++/player.cpp
+++ c++/player.cpp
@@ -26,9 +26,28 @@ class Player
     int jail_time = 3;
     bool smart_jail_strategy = false;
     int complete_monopoly = 0;
+    int[] group_preferences = new int[2];
     int development_threshold = 0;
 
     public:
-        Player(int num, int buy_thresh, int build_thresh, int jt, bool sjs, int cm, int dt);
-        void reset_values();
-}
+        Player(int num, int buy_thresh, int build_thresh, int jt, bool sjs, int cm, int dt)
+	{
+		number = num;
+		if (buy_thresh != NULL)
+			building_threshold = buy_thresh;
+		if (build_thresh != NULL)
+			building_threshold = build_thresh;
+		if (jt != NULL)
+			jail_time = jt;
+		if (sjs != NULL)
+			smart_jail_strategy = sjs;
+		if (cm != NULL)
+			complete_monopoly = cm;
+		if (dt != NULL)
+			development_threshold = dt;
+	}
+        void reset_values()
+	{
+
+	}
+};
