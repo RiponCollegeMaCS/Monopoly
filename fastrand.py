5dc70269cdb90f55888244341b3954b0ec82ed9b
Braxton Schafer <braxton.schafer@gmail.com>
2015-04-20 18:58:14 -0700

    Fix non-randomness for Windows

--- fastrand.py
+++ fastrand.py
@@ -24,19 +24,12 @@ if sys.platform == 'linux':
 elif sys.platform == 'darwin':
 	libc = ctypes.cdll.LoadLibrary("libSystem.dylib")
 elif sys.platform == 'win32':
-	# well crap
 	libc = ctypes.cdll.LoadLibrary("msvcrt.dll") # hopefully this'll work
 
-
-def _roll():
-    # Initialize and seed
-    libc.srand(libc.time(None))
-    while True:
-        yield libc.rand() % 6 + 1
-
-
 def roll():
-    return next(_roll())
+	libc.srand(libc.time(None) + libc.rand())
+	return libc.rand() % 6 + 1
+    # return next(_roll())
 
 def choice(lst):
     # Initialize and seed
@@ -50,3 +43,7 @@ def shuffle(lst):
     for i in range(len(lst)):
         j = libc.rand() % (len(lst) - i) + i
         lst[i], lst[j] = lst[j], lst[i]
+
+if __name__ == '__main__':
+	for i in range(100000):
+		print(roll())
