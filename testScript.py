fe84d64739d9fdde6386953429a336bfd56570a0
eithunm <eithunm@ripon.edu>
2015-06-12 14:21:04 -0500

    test changes2

--- testScript.py
+++ testScript.py
@@ -1,9 +1,8 @@
 import monopoly
+from timer import *
 
 def old_success_indicator(games_in_a_set=1000):
-	
     counter = 0
-
     for i in range(games_in_a_set):
         # Play game.
         player1 = monopoly.Player(1, buying_threshold=500)
@@ -16,3 +15,8 @@ def old_success_indicator(games_in_a_set=1000):
             counter += 1
 
     return counter / games_in_a_set
+
+if __name__ == '__main__':
+    timer()
+    print(old_success_indicator())
+    timer()
